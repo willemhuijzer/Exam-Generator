af93eaeca18ef24ba872618877e200c883f96ed7
harshpundhir <pundhirharsh6@gmail.com>
2023-05-10 20:39:13 +0200

    added

--- project/prompt_constants.py
+++ project/prompt_constants.py
@@ -24,3 +24,19 @@ Question 2:
 Generate 5 exam questions based on the provided CONTENT OF LECTURE SLIDES above in the format of the SAMPLE QUESTIONS:
 5 QUESTIONS: 
 '''
+
+# '''Which of the following is not a strategy parameter in EA?
+# a) mutation rate
+# b) crossover operator
+# c) selection mechanism
+# d) population color
+
+# EA parameters are rigid (values constant during a run).
+# a) True
+# b) False
+
+# Which of the following statements are true regarding varying mutation step size?
+# a) The constant σ can be replaced by a function σ(t) updated after every n steps by the 1/5 success rule.
+# b) The constant σ can be replaced by a function σ (t) = 1 - 0.9 × Tt, where t is the current generation number.
+# c) A personal σ can be assigned to each individual and incorporated into the chromosome.
+# d) A personal σ can be assigned to each variable in each individual and incorporated into the chromosome.'''
