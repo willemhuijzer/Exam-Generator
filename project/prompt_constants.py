184e97f5fc6b9a37c481bc14dcf5243025e48fc5
willemhuijzer <43013233+willemhuijzer@users.noreply.github.com>
2023-05-10 20:38:36 +0200

    changes

--- project/prompt_constants.py
+++ project/prompt_constants.py
@@ -20,7 +20,50 @@ Question 2:
     D. Improved predictability of parameter values
 
     Answer: C
-    
+
+
 Generate 5 exam questions based on the provided CONTENT OF LECTURE SLIDES above in the format of the SAMPLE QUESTIONS:
 5 QUESTIONS: 
 '''
+
+# Question 3:
+#     Which of the following is NOT a major type of parameter control in Evolutionary Algorithms?
+#     A. Deterministic
+#     B. Adaptive
+#     C. Self-adaptive
+#     D. Randomized
+
+#     Answer: D
+
+# Question 4:
+#     What is the main advantage of adaptive and self-adaptive parameter control in Evolutionary Algorithms?
+#     A. Increased user control
+#     B. Reduction in required computing resources
+#     C. Liberation from parameter tuning and delegating parameter setting to the evolutionary process
+#     D. Improved predictability of parameter values
+
+#     Answer: C
+
+
+# Question 5:
+#     Which of the following is NOT a major type of parameter control in Evolutionary Algorithms?
+#     A. Deterministic
+#     B. Adaptive
+#     C. Self-adaptive
+#     D. Randomized
+
+#     Answer: D
+
+# Question 6:
+#     What is the main advantage of adaptive and self-adaptive parameter control in Evolutionary Algorithms?
+#     A. Increased user control
+#     B. Reduction in required computing resources
+#     C. Liberation from parameter tuning and delegating parameter setting to the evolutionary process
+#     D. Improved predictability of parameter values
+
+# Question 7:
+# What is the main advantage of adaptive and self-adaptive parameter control in Evolutionary Algorithms?
+# A. Increased user control
+# B. Reduction in required computing resources
+# C. Liberation from parameter tuning and delegating parameter setting to the evolutionary process
+# D. Improved predictability of parameter values
